a9f6fca631a8b8acc94fd8e646c30e89e20b4b49
Riteka <ritekaschool@gmail.com>
2020-08-12 21:07:08 -0700

    Added DPR andCCWM to API Version

--- API Version.py
+++ API Version.py
@@ -51,12 +51,12 @@ def teams_least_to_greatest(json_file):
 	
 
 
-def initmatrix(num_of_teams, num_of_alliances, json_file, teams, OPR):
+def initmatrix(num_of_teams, num_of_alliances, json_file, teams, OPR_DPR):
 	M_array = np.zeros( ((num_of_alliances), num_of_teams), dtype = int ) #initializing a matrix of 0's with num_alliances rows and num_alliance columns
 	s_array = np.zeros( (num_of_alliances, 1) ) #initializing the alliance score matrix
 	row_num = 0
 	
-	if (OPR == 'OPR'):
+	if (OPR_DPR == 'OPR'):
 		for match in json_file:
 			if (match["comp_level"]=="qm"):
 				for x in range(0,3):			#x is to access the different teams within the red alliance in the specific match
@@ -73,6 +73,22 @@ def initmatrix(num_of_teams, num_of_alliances, json_file, teams, OPR):
 				s_array[row_num] = match["alliances"]["blue"]["score"]
 				row_num += 1
 
+	if (OPR_DPR == "DPR"):
+		for match in json_file:
+			if (match["comp_level"]=="qm"):
+				for x in range(0,3):			#x is to access the different teams within the red alliance in the specific match
+					z = int(match["alliances"]["red"]["team_keys"][x][3:])
+					index = teams.index(z)
+					M_array[row_num][index] = 1
+				s_array[row_num] = match["alliances"]["blue"]["score"]
+				row_num += 1
+
+				for y in range(0,3):		#y is to access the different teams within the blue alliance in the specific match
+					r = int(match["alliances"]["blue"]["team_keys"][y][3:])
+					index = teams.index(r)
+					M_array[row_num][index] = 1
+				s_array[row_num] = match["alliances"]["red"]["score"]
+				row_num += 1
 
 	return([M_array, s_array])
 
@@ -83,7 +99,7 @@ def isSquare (matrix):
 
 
 
-def OPR_Calc(initmatrixresults): 
+def OPR_or_DPR_Calc(initmatrixresults): 
 #since the number of matches (represents the number of equations) is greater than the number of teams in the event (represents the variables), overdetermined
 	M_array = initmatrixresults[0]		#since the initmatrix() function is being passed in, the individual parts of the list that initmatrix() returns, represents M_array and the s_array
 	s_array = initmatrixresults[1]
@@ -101,6 +117,9 @@ def OPR_Calc(initmatrixresults):
 
 	return(final_right_side)
 
+def CCWM_Calc(OPR, DPR):
+	CCWM = OPR - DPR
+	return (CCWM)
 
 def main():
 	matches_json = fetch_json('https://www.thebluealliance.com/api/v3/event/2019abca/matches')
@@ -112,16 +131,20 @@ def main():
 	teams = teams_least_to_greatest(matches_json)
 	#print(teams)
 	OPR_matrix = initmatrix(num_of_teams, num_of_alliances, matches_json, teams, "OPR")
-	OPR_results = OPR_Calc(OPR_matrix)
-	display(OPR_results, teams)
+	OPR_results = OPR_or_DPR_Calc(OPR_matrix)
+	DPR_matrix = initmatrix(num_of_teams, num_of_alliances, matches_json, teams, "DPR")
+	DPR_results = OPR_or_DPR_Calc(DPR_matrix)
+	display(OPR_results, DPR_results, teams)
 	
 
-def display(OPR_results, teams):
+def display(OPR_results, DPR_results, teams):
 	print
 	count = 0
 	for result in OPR_results:
 		OPR = float(result)
-		print (teams[count], "				", "OPR: ", float(result))
+		DPR = float(DPR_results[count])
+		CCWM = float(CCWM_Calc(result, DPR_results[count]))
+		print (teams[count], "				", "OPR: ", float(result), "					", "DPR: ", DPR, "					", "CCWM: ",CCWM)
 		count += 1
 
 
